f7786b6a40a9f956c5609e329bc8f15dc50d5d8b
motconkien <hynuiux@gmail.com>
2025-07-13 15:45:59 +0900

    updated

--- journal/views.py
+++ journal/views.py
@@ -5,6 +5,9 @@ from rest_framework import viewsets, permissions
 from .models import JournalEntry
 from .serializers import JournalEntrySerializer
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 class JournalEntryViewSet(viewsets.ModelViewSet):
     queryset = JournalEntry.objects.all()
@@ -22,3 +25,7 @@ class JournalEntryViewSet(viewsets.ModelViewSet):
         if not self.request.user.is_authenticated:
             raise PermissionDenied("You have to log in to create journal")
         serializer.save(user=self.request.user)
+
+    @action(detail=False, methods=['get'], url_path='mood-choices')
+    def mood_choices(self, request):
+        return Response(self.serializer_class.Meta.model.MOOD_CHOICES)
