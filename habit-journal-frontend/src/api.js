f246181044e143f197a38f9f94d7e1fadded7deb
motconkien <hynuiux@gmail.com>
2025-07-22 16:05:09 +0900

    updated .env

--- habit-journal-frontend/src/api.js
+++ habit-journal-frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api/'
+    // baseURL: 'http://localhost:8000/api/'
+    baseURL:process.env.REACT_APP_API_URL
 });
 
 export const setAuthToken = (token) => {
